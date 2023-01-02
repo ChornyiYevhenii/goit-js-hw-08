9d98c9bb0015d508784d2554bfc986ed348471fe
ChornyiYevhenii <gibridius16@gmail.com>
2023-01-02 19:12:10 +0200

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+const galleryMarkap = createGaleryMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML("beforeend", galleryMarkap);
+
+function createGaleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      (image) => `
+    <a class="gallery__item" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" 
+ />
+    </a>
+`
+    )
+    .join("");
+}
+
+const simpleLightboxOptions = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+let gallerySet = new SimpleLightbox(".gallery a", simpleLightboxOptions);
